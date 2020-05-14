64b87bc8d703c368294dd95f9e3151ed3162b28b
Huy Nguyen <ndquochuy257@yahoo.com>
2020-05-14 23:24:27 +0700

    Xpath-1

--- selenium/webdriver/Topic_02_Xpath_Locator.java
+++ selenium/webdriver/Topic_02_Xpath_Locator.java
@@ -1,5 +1,38 @@
 package webdriver;
 
-public class Topic_02_Xpath_Locator {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_02_Xpath_Locator {
+  
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://live.demoguru99.com/index.php/customer/account/login/");
+	}
+	
+	@Test
+    public void TC_01(){
+		WebElement createaccount = driver.findElement(By.xpath(".//*[@id='login-form']/div/div[1]/div[2]/a"));
+		createaccount.click();
+	}
+		
+	
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
 }
